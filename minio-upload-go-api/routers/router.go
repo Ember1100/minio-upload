24d3ac4c83d2d5aa40eab0ba9aa8a2cf29e96ae6
黄琦 <huangqi50569@hundsun.com>
2023-09-01 13:48:26 +0800

    添加跨域

--- minio-upload-go-api/routers/router.go
+++ minio-upload-go-api/routers/router.go
@@ -135,13 +135,28 @@ func Merge(c *gin.Context) {
 		"data":    nil,
 	})
 }
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", "*") // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
+	}
+}
+
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-	router.Use(func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Next()
-
-	})
+	router.Use(Cors())
 	// router.LoadHTMLGlob("./templates/*")
 	// router.GET("/demo", func(c *gin.Context) {
 	// 	c.HTML(http.StatusOK, "index.html", gin.H{})
